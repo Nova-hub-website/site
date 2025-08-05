e0b12bf649a045c4472599dfd8ddbb65d5dc6cf6
Nova-hub-website <nanaion.xp@gmail.com>
2025-08-05 09:23:23 +0000

    Orders2.01

--- backend/routes/customerRoutes.js
+++ backend/routes/customerRoutes.js
@@ -49,7 +49,7 @@ router.post("/login", async (req, res) => {
 router.post("/add-order/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    // const { order } = req.body;
+    const { order } = req.body;
 
     // Find customer and update order history
     const customer = await Customer.findById(id);
